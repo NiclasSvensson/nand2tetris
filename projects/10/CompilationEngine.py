1c641f19b46ef30d0ed6940f279131a25a6b3b6b
Niclas <niclas.o.a.svensson@gmail.com>
2022-07-19 16:44:36 +0200

    project 9: solved without expressions

--- projects/10/CompilationEngine.py
+++ projects/10/CompilationEngine.py
@@ -1,49 +1,157 @@
 class CompilationEngine:
     def __init__(self, input_stream, file_name):
         self.stream = input_stream
+        self.index = 0
         self.file = open(file_name, "w")
+        self.indent = "  "
+        self.types = ["int", "char", "boolean"]
+        self.statements = ["let", "do", "return", "if", "while"]
 
     def compileClass(self):
-        pass
+        indent = 0
+        self.writeTag("class", False, indent)
+        self.advance(3, indent)
+        while self.stream[self.index][0] == "static" or self.stream[self.index][0] == "field":
+            self.compileClassVarDec(indent+1)
+        while self.stream[self.index][0] == "constructor" or self.stream[self.index][0] == "function" or self.stream[self.index][0] == "method":
+            self.compileSubroutine(indent+1)
+        self.advance(1, indent)
+        self.writeTag("class", True, indent)
 
-    def compileClassVarDec(self):
-        pass
+    def compileClassVarDec(self, indent):
+        self.writeTag("classVarDec", False, indent)
+        self.advance(2, indent)
+        while self.stream[self.index][1] == "identifier":
+            self.advance(2, indent)
+        self.writeTag("classVarDec", True, indent)
 
-    def compileSubroutine(self):
-        pass
+    def compileSubroutine(self, indent):
+        self.writeTag("subroutineDec", False, indent)
+        self.advance(4, indent)
+        self.compileParameterList(indent+1)
+        self.advance(1, indent)
+        self.writeTag("subroutineBody", False, indent+1)
+        self.advance(1, indent+1)
+        while self.stream[self.index][0] == "var":
+            self.compileVarDec(indent+1)
+        self.compileStatements(indent+1)
+        self.advance(1, indent+1)
+        self.writeTag("subroutineBody", True, indent+1)
+        self.writeTag("subroutineDec", True, indent)
 
-    def compileParameterList(self):
-        pass
+    def compileParameterList(self, indent):
+        self.writeTag("parameterList", False, indent)
+        while self.stream[self.index][0] in self.types or self.stream[self.index][1] == "identifier":
+            self.advance(2, indent)
+            if self.stream[self.index][0] == ",":
+                self.advance(1, indent)
+        self.writeTag("parameterList", True, indent)
 
-    def compileVarDec(self):
-        pass
+    def compileVarDec(self, indent):
+        self.writeTag("varDec", False, indent+1)
+        self.advance(2, indent+1)
+        while self.stream[self.index][1] == "identifier":
+            self.advance(2, indent+1)
+        self.writeTag("varDec", True, indent+1)
 
-    def compileStatements(self):
-        pass
+    def compileStatements(self, indent):
+        self.writeTag("statements", False, indent+1)
+        while self.stream[self.index][0] in self.statements:
+            if self.stream[self.index][0] == "let":
+                self.compileLet(indent+1)
+            elif self.stream[self.index][0] == "do":
+                self.compileDo(indent+1)
+            elif self.stream[self.index][0] == "return":
+                self.compileReturn(indent+1)
+            elif self.stream[self.index][0] == "if":
+                self.compileIf(indent+1)
+            elif self.stream[self.index][0] == "while":
+                self.compileWhile(indent+1)
+        self.writeTag("statements", True, indent+1)
 
-    def compileDo(self):
-        pass
+    def compileDo(self, indent):
+        self.writeTag("doStatement", False, indent+1)
+        if self.stream[self.index+2][0] == ".":
+            self.advance(5, indent+1)
+        else:
+            self.advance(3, indent+1)
+        self.compileExpressionList(indent+1);
+        self.advance(2, indent+1)
+        self.writeTag("doStatement", True, indent+1)
 
-    def compileLet(self):
-        pass
+    def compileLet(self, indent):
+        self.writeTag("letStatement", False, indent+1)
+        self.advance(2, indent+1)
+        if self.stream[self.index][0] == "[":
+            self.advance(1, indent+1)
+            self.compileExpression(indent+1)
+            self.advance(1, indent+1)
+        self.advance(1, indent+1)
+        self.compileExpression(indent+1)
+        self.advance(1, indent+1)
+        self.writeTag("letStatement", True, indent+1)
+        #print(self.stream[self.index][0])
 
-    def compileWhile(self):
-        pass
+    def compileWhile(self, indent):
+        self.writeTag("whileStatement", False, indent+1)
+        self.advance(2, indent+1)
+        self.compileExpression(indent+1)
+        self.advance(2, indent+1)
+        self.compileStatements(indent+1)
+        self.advance(1, indent+1)
+        self.writeTag("whileStatement", True, indent+1)
 
-    def compileReturn(self):
-        pass
+    def compileReturn(self, indent):
+        self.writeTag("returnStatement", False, indent+1)
+        self.advance(1, indent+1)
+        if self.stream[self.index][0] != ";":
+            self.compileExpression(indent+1)
+        self.advance(1, indent+1)
+        self.writeTag("returnStatement", True, indent+1)
 
-    def compileIf(self):
-        pass
+    def compileIf(self, indent):
+        self.writeTag("ifStatement", False, indent+1)
+        self.advance(2, indent+1)
+        self.compileExpression(indent+1)
+        self.advance(2, indent+1)
+        self.compileStatements(indent+1)
+        self.advance(1, indent+1)
+        if self.stream[self.index][0] == "else":
+            self.advance(2, indent+1)
+            self.compileStatements(indent+1)
+            self.advance(1, indent+1)
+        self.writeTag("ifStatement", True, indent+1)
 
-    def compileExpression(self):
-        pass
+    def compileExpression(self, indent):
+        self.writeTag("expression", False, indent+1)
+        self.compileTerm(indent+1)
+        self.writeTag("expression", True, indent+1)
 
-    def compileIf(self):
-        pass
+    def compileTerm(self, indent):
+        self.writeTag("term", False, indent+1)
+        self.advance(1, indent+1)
+        self.writeTag("term", True, indent+1)
 
-    def compileTerm(self):
-        pass
+    def compileExpressionList(self, indent):
+        self.writeTag("expressionList", False, indent+1)
+        while self.stream[self.index][0] != ")":
+            self.compileExpression(indent+1)
+            if self.stream[self.index][0] == ",":
+                self.advance(1, indent+1)
+            else:
+                break
+        self.writeTag("expressionList", True, indent+1)
 
-    def compileExpressionList(self):
-        pass
+    def advance(self, increment, indent):
+        for i in range(increment):
+            self.writeTerminal(self.stream[self.index][0], self.stream[self.index][1], indent + 1)
+            self.index += 1
+
+    def writeTerminal(self, symbol, type, indent):
+        symbol = "&lt;" if symbol == "<" else symbol
+        symbol = "&gt;" if symbol == ">" else symbol
+        symbol = "&amp;" if symbol == "&" else symbol
+        self.file.write(self.indent*indent + "<" + type + "> " + symbol + " </" + type + ">\n")
+
+    def writeTag(self, word, end, indent):
+        self.file.write(self.indent*indent + ("<" + word + ">\n" if not end else "</" + word + ">\n"))
--- projects/10/JackAnalyzer.py
+++ projects/10/JackAnalyzer.py
@@ -11,16 +11,18 @@ def main(args):
         while tokenizer.hasMoreTokens():
             token_type = tokenizer.tokenType()
             if token_type == "KEYWORD":
-                input_stream.append(tokenizer.keyWord())
+                input_stream.append((tokenizer.keyWord(), "keyword"))
             elif token_type == "SYMBOL":
-                input_stream.append(tokenizer.symbol())
+                input_stream.append((tokenizer.symbol(), "symbol"))
             elif token_type == "IDENTIFIER":
-                input_stream.append(tokenizer.identifier())
+                input_stream.append((tokenizer.identifier(), "identifier"))
             elif token_type == "INT_CONST":
-                input_stream.append(tokenizer.intVal())
+                input_stream.append((tokenizer.intVal(), "integerConstant"))
             elif token_type == "STRING_CONST":
-                input_stream.append(tokenizer.stringVal())
+                input_stream.append((tokenizer.stringVal(), "stringConstant"))
         compilation_engine = CompilationEngine(input_stream, os.path.join(args.jack_path, tokenizer.getFileName() + "_my.xml"))
+        #print(input_stream)
+        compilation_engine.compileClass()
         tokenizer.close()
 
 if __name__ == '__main__':
